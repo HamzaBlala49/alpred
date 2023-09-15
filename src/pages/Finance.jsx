6c314003321d51dd7cf601a1b4cea1a58a945d00
HamzaBlala49 <103255525+HamzaBlala49@users.noreply.github.com>
2023-09-15 15:14:12 -0700

    modify Finance filter

--- src/pages/Finance.jsx
+++ src/pages/Finance.jsx
@@ -1,7 +1,7 @@
 import React, { useState ,useEffect} from 'react'
 import { useAuthHeader, useIsAuthenticated } from 'react-auth-kit';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAdd,faMoneyBill,faInfoCircle,faTrashCan, faPenToSquare,faStore ,faBuilding} from '@fortawesome/free-solid-svg-icons';
+import { faAdd,faMoneyBill,faSearch,faInfoCircle,faTrashCan, faPenToSquare,faStore ,faBuilding} from '@fortawesome/free-solid-svg-icons';
 import Loader from '../components/Loader';
 import { bisUrl } from '../context/biseUrl';
 import Confirm from '../components/Confirm';
@@ -18,7 +18,16 @@ function Finance() {
   let [isLoad, setIsLoad] = useState(false);
   let [element,setElement] = useState(null);
   let [searchValue,setSearchValue] = useState("")
-  let [selectValue,setSelectValue] = useState("old");
+  let [offices,setOffices] = useState([]);
+  let [officeId,setOfficeId] = useState("");
+  let [type_currency,setType_currency] = useState("");
+  let [type_account,setType_account] = useState("");
+  let [start_date,setStart_date] = useState("");
+  let [end_date,setEnd_date] = useState("");
+  let [total,setTotal] = useState(null);
+
+
+
   let [note,setNote] = useState("");
 
   const authHeader = useAuthHeader()
@@ -34,8 +43,7 @@ function Finance() {
     setIsLoad(true)
     if(isauth()){
       axios.get(`${bisUrl}/office/finamcefunds/`,config).then(res=>{
-        setData(res.data);
-        console.log(res.data);
+        setData(res.data.reverse());
         setIsLoad(false)
 
       }).catch(e=>{
@@ -43,16 +51,15 @@ function Finance() {
 
       alert("حث خطأ اثناء جلب البيانات تأكد من اتصالك بالشبكة")
       })
+
+      axios.get(`${bisUrl}/office/office/`,config).then(res=>{
+        setOffices(res.data.reverse());
+      }).catch(e=>{
+        alert("حث خطأ اثناء جلب البيانات تأكد من اتصالك بالشبكة")
+      })
     }
   },[])
   
-  useEffect(()=>{
-    if(selectValue == "new"){
-      setData([...data.reverse()]);
-    }else{
-      setData([...data.reverse()]);
-    }
-  },[selectValue])
 
   let handelElement = (el)=>{
     setElement(el)
@@ -81,20 +88,69 @@ function Finance() {
       setSearchValue(e.target.value);
   }
 
-  let handelChangeSelect = (e)=>{
-    setSelectValue(e.target.value)
-  }
   let handelNote = (note)=>{
     setNote(note)
     
-}
+  }
+
+  let handelResetting = (el) =>{
+    setEnd_date("");
+    setStart_date("");
+    setType_account("");
+    setType_currency("");
+    setOfficeId("");
+    setTotal(null);
+
+    setIsLoad(true)
+    if(isauth()){
+      axios.get(`${bisUrl}/office/finamcefunds/`,config).then(res=>{
+        setData(res.data.reverse());
+        setIsLoad(false)
+
+      }).catch(e=>{
+        console.error(e)
+
+      alert("حث خطأ اثناء جلب البيانات تأكد من اتصالك بالشبكة")
+      })
+    }
+  }
+
+  let handelChangeType_currency=(e)=>{
+    setType_currency(e.target.value);
+    if(type_currency == ""){
+      setTotal(null)
+    }
+
+  }
+
+  let handelChangeType_account=(e)=>{
+    setType_account(e.target.value);
+    if(type_account == ""){
+      setTotal(null)
+    }
+
+  }
+
+  let handelSearch = ()=>{
+    if(isauth){
+      setIsLoad(true)
+      axios.get(`${bisUrl}/office/finamcefunds/?office=${officeId}&type_currency=${type_currency}&type_account=${type_account}&start_date=${start_date}&end_date=${end_date}`,config).then((res)=>{
+        setData(res.data.reverse());
+        setTotal(res.data[0].total_price)
+        setIsLoad(false)
+
+        }).catch((e)=>{
+          alert("حدث خطأ أثناء عملية البحث")
+        })
+  }
+  }
 
   return (
    <div className='p-2'>
 
     <h6 className='text-dark'><FontAwesomeIcon icon={faMoneyBill} /> إدارة المالية </h6>
 
-    <div className='row g-1 mt-3'>
+    <div className='row g-3 mt-3'>
 
       <div className='col-12 col-lg-1 col-md-3 col-sm-12'>
       <Link to={'/office_home'} className="btn btn-outline-dark btn-sm w-100" style={{fontSize:'14px'}} role="button">رجوع</Link>
@@ -108,8 +164,7 @@ function Finance() {
           </div>
       }
 
-      <div className='col-12 col-lg-3 col-md-3 col-sm-12'>
-        
+      <div className='col-12 col-lg-3 col-md-3 col-sm-12'>      
       <input 
         onChange={(e)=>  handelChangeSearchValue(e)} 
         type="text" 
@@ -119,13 +174,87 @@ function Finance() {
       </div>
 
       <div className='col-12 col-lg-2 col-md-2 col-sm-12'>
-        <select onChange={(e)=> handelChangeSelect(e)} value={selectValue} className="form-select form-select-sm"
+        <select onChange={(e)=> setOfficeId(e.target.value)} value={officeId} className="form-select form-select-sm"
         style={{fontSize:'14px'}} 
         id="floatingSelectGrid">
-            <option value="old">قديم</option>
-            <option value="new">جديد</option>
+            <option value="">كل المكاتب</option>
+            {
+              offices.map((el)=><option value={el.id}>{el.name}</option>)
+            }
+            
         </select>
       </div>
+
+      <div className='col-12 col-lg-2 col-md-2 col-sm-12'>
+        <select onChange={(e)=> handelChangeType_currency(e)} value={type_currency} className="form-select form-select-sm"
+        style={{fontSize:'14px'}} 
+        id="floatingSelectGrid">
+            <option value="">كل أنواع العملات</option>
+            <option value="1">ريال يمني عملة عدن</option>
+            <option value="2">ريال سعودي</option>
+            <option value="3">دولار</option>
+            <option value="4">ريال يمني عملة صنعاء</option>
+            
+        </select>
+      </div>
+
+      <div className='col-12 col-lg-2 col-md-2 col-sm-12'>
+        <select onChange={(e)=> handelChangeType_account(e)} value={type_account} className="form-select form-select-sm"
+        style={{fontSize:'14px'}} 
+        id="floatingSelectGrid">
+            <option value="">كل انواع الحسابات</option>
+            <option value="1">إراد</option>
+            <option value="2">مصروفات</option>
+            <option value="3">مشتريات</option>
+            <option value="4">مبيعات</option>
+            <option value="5">رأس مال</option>
+            
+        </select>
+      </div>
+
+      <div className='col-12 col-lg-3 col-md-3 col-sm-12'>
+        <div className='d-flex'>
+            <label style={{fontSize:"14px"}}>من تاريخ:</label>
+            <input type="date" onChange={(e)=> setStart_date(e.target.value)} value={start_date} className='form-control  form-control-sm outline-none' style={{fontSize:"14px",height:"30px"}}/>
+        </div>
+           
+      </div>
+
+      
+      <div className='col-12 col-lg-3 col-md-3 col-sm-12'>
+        <div className='d-flex'>
+            <label style={{fontSize:"14px"}}>الئ تاريخ:</label>
+            <input type="date" onChange={(e)=> setEnd_date(e.target.value)} value={end_date} className='form-control  form-control-sm outline-none' style={{fontSize:"14px",height:"30px"}}/>
+        </div>
+           
+      </div>
+
+
+     
+
+      <div className='col-12 col-lg-2 col-md-2 col-sm-12'>
+            <button className='btn btn-sm btn-secondary w-100' onClick={(e)=> handelResetting()} style={{fontSize:'14px',fontWeight:"bold"}}>إعاده ضبط</button>
+      </div>
+
+      {
+          officeId != "" || type_account !="" || type_currency  != "" || start_date != "" || end_date != "" ?  <>
+          <div className='col-12 col-lg-2 col-md-2 col-sm-12'>
+            <button className='btn btn-sm btn-dark w-100' onClick={(e)=> handelSearch()} style={{fontSize:'14px',fontWeight:"bold"}}><FontAwesomeIcon icon={faSearch} /> بحث</button>
+          </div>
+          </>
+          :
+          null
+      }
+
+      {
+          type_account !="" || type_currency  != ""  && total !=null?  <>
+          <div className='col-12 col-lg-5 col-md-5 col-sm-12'>
+            <p style={{fontSize:"14px"}}>المجموع الكلي :<b>{total || "لا يوجد"}</b></p>
+          </div>
+          </>
+          :
+          null
+      }
 
     </div>
 
@@ -152,8 +281,8 @@ function Finance() {
         <tbody>
           { data.map((el,index)=>{
 
-            return el.name_office?.startsWith(searchValue) ? <tr key={index}>
-            <th scope="row">{selectValue =="old" ? index+1 : data.length - index}</th>
+            return el.expulsion?.toString().startsWith(searchValue) ? <tr key={index}>
+            <th scope="row">{data.length - index}</th>
             <td>{el.expulsion || <span className='text-warning'>لايوجد</span>}</td>
             <td>{el.name_office}</td>
             <td>{el.name_type_account}</td>
